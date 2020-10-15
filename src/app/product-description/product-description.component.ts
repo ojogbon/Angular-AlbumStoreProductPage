7804f35a35c8d4c2b467086993fc5448f10b9a80
ojogbon <40061054+ojogbon@users.noreply.github.com>
2020-10-15 11:57:56 -0700

    create a service to get album data edit

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,19 +7,17 @@ import {ProductService} from '../product.service';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
- albumInfo;
+ 
+  albumInfo;
   
-  private _productService_ : ProductService;
-
+  
   constructor(private _productService : ProductService) { 
-      this._productService_ = _productService;
   }
 
   ngOnInit() {
-    const tg = this._productService_.getAlbum(1)
+    this._productService.getAlbum(1)
     .subscribe(response => 
-      {this.albumInfo = response;
-    });
+    this.albumInfo = response);
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,23 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Response } from '@angular/http';
+import { Http , Response} from '@angular/http';
+ 
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
-  private _http_: Http;
-
   private _albumUrl: string = "../assets/album.json";  
 
   constructor(private _http: Http) {
-      this._http_ = _http;
    }
 
    public getAlbum (id: number){
-      return this._http_.get(this._albumUrl)
-      .map(responce => {
-        return responce.json()});
+      return this._http.get(this._albumUrl)
+      .map((responce) =>  responce.json());
    }
 
    
