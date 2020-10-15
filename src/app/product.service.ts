15d405af6d7e19469c5444e75540ae920ffe77ea
ojogbon <40061054+ojogbon@users.noreply.github.com>
2020-10-15 13:57:14 -0700

    use interfaces to describe data in this album project

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Http , Response} from '@angular/http';
- 
+import {Observable} from 'rxjs/Observable';
+import {Album} from './album';
+
 import 'rxjs/add/operator/map';
+
+
 
 @Injectable()
 export class ProductService {
@@ -10,9 +14,9 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum (id: number){
+  getAlbum (id: number) : Observable<Album> {
       return this._http.get(this._albumUrl).map((response) =>
-        response.json());
+        <Album> response.json());
    }
 
    
---
+++ src/app/track.ts
@@ -0,0 +1,8 @@
+export interface Track {
+
+    trackNumber : number;
+    trackName : string;
+    trackLenght : string;
+    trackPrice : number;
+
+}
