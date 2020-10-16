7397cf3aecadfb5a72fede3e2f91041375984712
ojogbon <40061054+ojogbon@users.noreply.github.com>
2020-10-15 21:52:10 -0700

    final complete  navigation using the angular router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,21 +12,11 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 import { from } from 'rxjs/observable/from';
 
-const appRoutes : Routes = [
-    {
-      path : 'products',
-      component : ProductListComponent
-    },
-    {
-      path : 'products/:id',
-      component : ProductPageComponent
-    },
-    {
-      path : '',
-      redirectTo : "products",
-      pathMatch : "full"
-    }
-] ;
+const appRoutes: Routes = [
+    { path : 'products', component : ProductListComponent },
+    { path : 'product/:id', component : ProductPageComponent },
+    { path : '',redirectTo : "products",pathMatch : "full" }
+];
 
 
 @NgModule({
